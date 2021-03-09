990686d6c9be3be3a13ba7339eb016b4333fc856
echedey-luis-alvarez <80125792+echedey-luis-alvarez@users.noreply.github.com>
2021-03-09 10:20:06 +0100

    Add Arduino projects files

---
+++ Robot_CMC/Robot_CMC.ino
@@ -0,0 +1,13 @@
+/*  Project: CarMap - Chikito
+ *  This file is the main program for the robot Chikito.
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ Teclado_macros/Teclado_macros.ino
@@ -0,0 +1,13 @@
+/*  Project: CarMap - Chikito
+ *  This file is the main program for the macro keyboard.
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
