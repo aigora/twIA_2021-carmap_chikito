d39b25db90e338b8fbc0daa8960d220707f84891
echedey-luis-alvarez <80125792+echedey-luis-alvarez@users.noreply.github.com>
2021-03-11 11:51:21 +0100

    Create Mapeador.c

---
+++ Mapeador/Mapeador.c
@@ -0,0 +1,10 @@
+/*  Mapeador.cpp : This file contains the 'main' function. Program execution begins and ends there.
+*/
+
+#include <stdio.h>
+
+int main() // Main function
+{
+    // This is where fun BEGINS
+    return 0;
+}
