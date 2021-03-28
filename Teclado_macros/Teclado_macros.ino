76b6cbecdfdbabb0eb9b77886288aca96cc3046a
Amélie <79919225+amelie-nader-prieto@users.noreply.github.com>
2021-03-28 21:14:04 +0200

    Update Teclado_macros.ino

--- Teclado_macros/Teclado_macros.ino
+++ Teclado_macros/Teclado_macros.ino
@@ -1,15 +1,42 @@
+
 /*  Project: CarMap - Chikito
  *  This file is the main program for the macro keyboard.
  */
 
-void setup() { //inicializando el teclado
-pinMode(button_pin,INPUT_PULLUP);
-Mouse.begin();
-Keyboard.begin();
-delay(1000)
+#include <Keyboard.h>
+#include <Servo.h>
+//#include <stdio.h>
+
+#define X A0    
+#define Y A1
+#define SW 2    //(press joystick: pause movement)
+#define KEY_Q 3 //"quit" button
+#define KEY_P 4 //switch (for profiles)
+#define BUTTON_5 5 //("start/end" button)
+#define BUTTON_6 6 //("save" button)
+/*#define BUTTON_7 7 //
+#define BUTTON_8 8 
+#define BUTTON_9 9 */ //not used
+
+void setup() {
+  pinMode(X,INPUT);
+  pinMode(Y,INPUT);
+  pinMode(SW,INPUT_PULLUP);
+  pinMode(KEY_Q,INPUT_PULLUP);
+  pinMode(KEY_P,INPUT_PULLUP);
+  pinMode(BUTTON_5,INPUT_PULLUP);
+  pinMode(BUTTON_6,INPUT_PULLUP);
+  /*pinMode(BUTTON_7,INPUT_PULLUP);
+  pinMode(BUTTON_8,INPUT_PULLUP);
+  pinMode(BUTTON_9,INPUT_PULLUP);*/
 }
 
+/*struct data_in{
+double anglerad;
+int position;
+int v;
+};*/
+
 void loop() {
-  int value=digitalRead(button_pin);
-  //aquí irá la función del teclado
+  //here comes the main function
 }
