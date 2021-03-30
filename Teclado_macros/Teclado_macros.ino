f6d8e77a875749d3bac7e57804ebc21d4126dd31
Amélie <79919225+amelie-nader-prieto@users.noreply.github.com>
2021-03-30 22:39:10 +0200

    update code with keyboard commands

--- Teclado_macros/Teclado_macros.ino
+++ Teclado_macros/Teclado_macros.ino
@@ -6,28 +6,35 @@
 #include <Keyboard.h>
 #include <HID.h>
 
-#define X A0    
+#define X A0        //joystick reads analog signals
 #define Y A1
-#define SW 2    //(press joystick: pause movement)
-#define KEY_Q 3 //"quit" button
-#define KEY_P 4 //"switch" (for profiles)
-#define BUTTON_R 5 //("start/end" button)
-#define BUTTON_S 6 //("save" button)
+#define SW 2        //(press joystick: pause movement)
+#define KEY_Q 3     //"quit"
+/*#define KEY_P 4*/ //"switch"
+#define BUTTON_R 5  //("start/end" button)
+#define BUTTON_S 6  //("save" button)
+#define BUTTON_M 7  //"move" button (which is pressed and held down);
 
 void setup() {
   pinMode(X,INPUT);
   pinMode(Y,INPUT);
   pinMode(SW,INPUT_PULLUP);
   pinMode(KEY_Q,INPUT_PULLUP);
-  pinMode(KEY_P,INPUT_PULLUP);
+  /*pinMode(KEY_P,INPUT_PULLUP);*/
   pinMode(BUTTON_R,INPUT_PULLUP);
   pinMode(BUTTON_S,INPUT_PULLUP);
+  pinMode(BUTTON_M,INPUT_PULLUP);
   Keyboard.begin();
 }
 
 void loop() {
+  int start=digitalRead(BUTTON_R);
+  while (start==HIGH){
+    delay(30);
+    start=digitalRead(BUTTON_R);
+  }
   int quit_key=digitalRead(KEY_Q);
-  int q_prev==HIGH;
+  int q_def=HIGH;
   do{
    int save=digitalRead(BUTTON_S);
    int s_prev=HIGH;
@@ -36,38 +43,69 @@ void loop() {
       save=digitalRead(BUTTON_S);
       if (save==LOW){
         Keyboard.press(KEY_LEFT_CTRL);
-        Keyboard.press('S');
+        Keyboard.press('s');
         delay(50);
         Keyboard.releaseAll();
       }
-      s_prev=save;
     }
+    s_prev=save;
     delay(30);
-    int start_end=digitalRead(BUTTON_R);
-    int r_prev==HIGH;
-    if ((start_end==LOW)&&(r_prev==HIGH)){
+    int finish=digitalRead(BUTTON_R);
+    int pause=digitalRead(SW);
+    int sw_prev=HIGH;
+    while ((pause==HIGH)&&(sw_prev==HIGH))
+    {
+      int move=digitalRead(BUTTON_M);
+      int m_prev=HIGH;
       delay(30);
-      start_end=digitalRead(BUTTON_R);
-      if (start_end==LOW){
-        //commands for start/end function;
+      while ((move==LOW)&&(pause==HIGH)){
+        int t=0; //time that key remains pressed
+        Keyboard.press(KEY_UP_ARROW);
+        delay(50);
+        move=digitalRead(BUTTON_M);
+        pause=digitalRead(SW);
+        t+=50; //time counter increases by 50ms
+        m_prev=move; 
       }
-      r_prev=start_end;
+      if m_prev==LOW{
+        Keyboard.release(KEY_UP_ARROW);
+      }
+      //Keyboard.println(t);
     }
-    delay(30);
+    if (pause==LOW)&&(sw_prev==HIGH)){
+      delay(30);
+      pause=digitalRead(SW);
+      if (pause==LOW){
+        Keyboard.press(KEY_ESC);
+        delay(50);
+        Keyboard.release(KEY_ESC); //pressing Escape key once leaves Pause menu activated until pressed again (?)
+        do{
+          int un_pause=digitalRead(SW);
+        }
+        while (un_pause==HIGH);
+        if (un_pause==LOW){
+          Keyboard.press(KEY_ESC);
+          delay(50);
+          Keyboard.release(KEY_ESC); //closes pause menu pressing Escape again
+        }
+      }
+    }
   }
   while (quit_key==HIGH);
-  if ((quit_key==LOW)&&(q_prev==HIGH)){
+  if ((quit_key==LOW)&&(q_def==HIGH)){
     delay(30);
     quit_key=digitalRead(KEY_Q);
     if quit_key==LOW{ 
       Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.press(KEY_F4);
+      Keyboard.press(KEY_F4);  //a classic killing technique
       delay(50);
-      Keyboard.releaseAll(); 
+      Keyboard.releaseAll();
+      /*Keyboard.println("se ha cerrado el programa");*/
       Keyboard.end();
     }
     else{
-      q_prev=quit_key;
+      quit_key==HIGH;
+      q_def=quit_key;
     }
   }
 }
