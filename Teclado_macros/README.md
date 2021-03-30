5721c4ced8ad2374083b85e3fd15e50f1e474fc5
Amélie <79919225+amelie-nader-prieto@users.noreply.github.com>
2021-03-30 09:07:43 +0200

    update readme and update Teclado_macros.ino

--- Teclado_macros/README.md
+++ Teclado_macros/README.md
@@ -1,6 +1,10 @@
 ## Programa del teclado Arduino
 Objetivo: Crear una interfaz (HID) para controlar el robot y realizar comandos, además de generar los datos de posición y rotación que necesitan los programas mapeador y robot CMC para funcionar.
 Se conecta en el puerto USB.
+El código emplea la biblioteca __Keyboard.h__;
+Funciones principales:
+- Lectura digital: pulsadores
+- Lectura analógica: _joystick_
 
 ---
 
--- Teclado_macros/Teclado_macros.ino
+++ Teclado_macros/Teclado_macros.ino
@@ -4,19 +4,15 @@
  */
 
 #include <Keyboard.h>
-#include <Servo.h>
-//#include <stdio.h>
+#include <HID.h>
 
 #define X A0    
 #define Y A1
 #define SW 2    //(press joystick: pause movement)
 #define KEY_Q 3 //"quit" button
-#define KEY_P 4 //switch (for profiles)
-#define BUTTON_5 5 //("start/end" button)
-#define BUTTON_6 6 //("save" button)
-/*#define BUTTON_7 7 //
-#define BUTTON_8 8 
-#define BUTTON_9 9 */ //not used
+#define KEY_P 4 //"switch" (for profiles)
+#define BUTTON_R 5 //("start/end" button)
+#define BUTTON_S 6 //("save" button)
 
 void setup() {
   pinMode(X,INPUT);
@@ -24,19 +20,54 @@ void setup() {
   pinMode(SW,INPUT_PULLUP);
   pinMode(KEY_Q,INPUT_PULLUP);
   pinMode(KEY_P,INPUT_PULLUP);
-  pinMode(BUTTON_5,INPUT_PULLUP);
-  pinMode(BUTTON_6,INPUT_PULLUP);
-  /*pinMode(BUTTON_7,INPUT_PULLUP);
-  pinMode(BUTTON_8,INPUT_PULLUP);
-  pinMode(BUTTON_9,INPUT_PULLUP);*/
+  pinMode(BUTTON_R,INPUT_PULLUP);
+  pinMode(BUTTON_S,INPUT_PULLUP);
+  Keyboard.begin();
 }
 
-/*struct data_in{
-double anglerad;
-int position;
-int v;
-};*/
-
 void loop() {
-  //here comes the main function
+  int quit_key=digitalRead(KEY_Q);
+  int q_prev==HIGH;
+  do{
+   int save=digitalRead(BUTTON_S);
+   int s_prev=HIGH;
+    if ((save==LOW)&&(s_prev==HIGH)){
+      delay(30);
+      save=digitalRead(BUTTON_S);
+      if (save==LOW){
+        Keyboard.press(KEY_LEFT_CTRL);
+        Keyboard.press('S');
+        delay(50);
+        Keyboard.releaseAll();
+      }
+      s_prev=save;
+    }
+    delay(30);
+    int start_end=digitalRead(BUTTON_R);
+    int r_prev==HIGH;
+    if ((start_end==LOW)&&(r_prev==HIGH)){
+      delay(30);
+      start_end=digitalRead(BUTTON_R);
+      if (start_end==LOW){
+        //commands for start/end function;
+      }
+      r_prev=start_end;
+    }
+    delay(30);
+  }
+  while (quit_key==HIGH);
+  if ((quit_key==LOW)&&(q_prev==HIGH)){
+    delay(30);
+    quit_key=digitalRead(KEY_Q);
+    if quit_key==LOW{ 
+      Keyboard.press(KEY_LEFT_ALT);
+      Keyboard.press(KEY_F4);
+      delay(50);
+      Keyboard.releaseAll(); 
+      Keyboard.end();
+    }
+    else{
+      q_prev=quit_key;
+    }
+  }
 }
