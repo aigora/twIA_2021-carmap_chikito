ee832f4deffbe5e0792017d4e159cdb2b036891f
Amélie <79919225+amelie-nader-prieto@users.noreply.github.com>
2021-04-02 18:23:19 +0200

    Create Teclado_macros.c

---
+++ Teclado_macros/Teclado_macros.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*#include "Keyboard.h"
+#include "robot.h"
+#include "vector.h"*/
+
+enum perfiles {
+	Perfil1,
+	Perfil2
+};
+
+int main(void) {
+	//configuración (setup);
+	//bucle principal
+	//cierre
+	;
+}
