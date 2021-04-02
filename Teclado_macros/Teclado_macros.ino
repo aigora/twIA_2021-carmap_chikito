9375c339d9279b20bff009121af699dec3a7b815
Amélie <79919225+amelie-nader-prieto@users.noreply.github.com>
2021-04-02 18:07:54 +0200

    Update Teclado_macros.ino

--- Teclado_macros/Teclado_macros.ino
+++ Teclado_macros/Teclado_macros.ino
@@ -4,108 +4,107 @@
  */
 
 #include <Keyboard.h>
-#include <HID.h>
 
-#define X A0        //joystick reads analog signals
-#define Y A1
-#define SW 2        //(press joystick: pause movement)
-#define KEY_Q 3     //"quit"
-/*#define KEY_P 4*/ //"switch"
-#define BUTTON_R 5  //("start/end" button)
-#define BUTTON_S 6  //("save" button)
-#define BUTTON_M 7  //"move" button (which is pressed and held down);
+#define X A0        
+#define Y A1        //joystick pins: analog
+#define PAUSE 2        //(press joystick: pause movement)
+#define QUIT 3     //"quit"
+#define SWITCH 4     //"switch"
+#define START 5  //("start/end" button)
+#define SAVE 6  //("save" button)
+#define FWD 7  //"move" button (which is pressed and held down);
+#define TURN 8  //"return" button
 
 void setup() {
+  const int button_pins[]={PAUSE,QUIT,SWITCH,START,SAVE,FWD,TURN};
+  int size=(sizeof(button_pins)/sizeof(button_pins[0]));
+  int j=0;
+  for (j=0;j<size;j++){
+    pinMode(button_pins[j],INPUT_PULLUP);
+  }
   pinMode(X,INPUT);
   pinMode(Y,INPUT);
-  pinMode(SW,INPUT_PULLUP);
-  pinMode(KEY_Q,INPUT_PULLUP);
-  /*pinMode(KEY_P,INPUT_PULLUP);*/
-  pinMode(BUTTON_R,INPUT_PULLUP);
-  pinMode(BUTTON_S,INPUT_PULLUP);
-  pinMode(BUTTON_M,INPUT_PULLUP);
   Keyboard.begin();
 }
 
-void loop() {
-  int start=digitalRead(BUTTON_R);
-  while (start==HIGH){
-    delay(30);
-    start=digitalRead(BUTTON_R);
+void loop(){
+  const int buttons[]={PAUSE,QUIT,SWITCH,START,SAVE,FWD,TURN};
+  int start=digitalRead(START);
+  if (start==HIGH){
+    do{
+      delay(30);
+      start=digitalRead(START);
+    } while (start==HIGH);
   }
-  int quit_key=digitalRead(KEY_Q);
-  int q_def=HIGH;
-  do{
-   int save=digitalRead(BUTTON_S);
-   int s_prev=HIGH;
-    if ((save==LOW)&&(s_prev==HIGH)){
+  Keyboard.press(KEY_KEYPAD_ENTER);
+  delay(50);
+  Keyboard.release(KEY_KEYPAD_ENTER);
+  bool buttons_def[]={HIGH,HIGH,HIGH,HIGH,HIGH,HIGH,HIGH};
+  bool buttons_now[]={HIGH,HIGH,HIGH,HIGH,HIGH,HIGH,HIGH};
+  int dim=sizeof(buttons)/sizeof(int);
+  int i=0;
+  for(i=0;i<dim;i++){
+    int press=digitalRead(buttons[i]);
+    if (press==LOW){
       delay(30);
-      save=digitalRead(BUTTON_S);
-      if (save==LOW){
-        Keyboard.press(KEY_LEFT_CTRL);
-        Keyboard.press('s');
-        delay(50);
-        Keyboard.releaseAll();
-      }
-    }
-    s_prev=save;
-    delay(30);
-    int finish=digitalRead(BUTTON_R);
-    int pause=digitalRead(SW);
-    int sw_prev=HIGH;
-    while ((pause==HIGH)&&(sw_prev==HIGH))
-    {
-      int move=digitalRead(BUTTON_M);
-      int m_prev=HIGH;
-      delay(30);
-      while ((move==LOW)&&(pause==HIGH)){
-        int t=0; //time that key remains pressed
-        Keyboard.press(KEY_UP_ARROW);
-        delay(50);
-        move=digitalRead(BUTTON_M);
-        pause=digitalRead(SW);
-        t+=50; //time counter increases by 50ms
-        m_prev=move; 
-      }
-      if m_prev==LOW{
-        Keyboard.release(KEY_UP_ARROW);
-      }
-      //Keyboard.println(t);
-    }
-    if (pause==LOW)&&(sw_prev==HIGH)){
-      delay(30);
-      pause=digitalRead(SW);
-      if (pause==LOW){
-        Keyboard.press(KEY_ESC);
-        delay(50);
-        Keyboard.release(KEY_ESC); //pressing Escape key once leaves Pause menu activated until pressed again (?)
-        do{
-          int un_pause=digitalRead(SW);
-        }
-        while (un_pause==HIGH);
-        if (un_pause==LOW){
+      press=digitalRead(buttons[i]);
+      if (press==LOW){
+        buttons_now[i]=LOW;
+        switch[i]{
+          case 2:
           Keyboard.press(KEY_ESC);
           delay(50);
-          Keyboard.release(KEY_ESC); //closes pause menu pressing Escape again
+          Keyboard.release(KEY_ESC);
+          break;
+          case 3:
+          Keyboard.press(KEY_LEFT_ALT);
+          Keyboard.press(KEY_F4);
+          delay(50);
+          Keyboard.release(KEY_LEFT_ALT);
+          Keyboard.release(KEY_F4);
+          break;
+          case 4:
+          break;
+          case 5:
+          Keyboard.press(KEY_KEYPAD_ENTER);
+          delay(50);
+          Keyboard.release(KEY_KEYPAD_ENTER);
+          break;
+          case 6:
+          Keyboard.press(KEY_RIGHT_CTRL);
+          Keyboard.press('s');
+          delay(50);
+          Keyboard.release(KEY_RIGHT_CTRL);
+          Keyboard.release('s');
+          break;
+          case 7:
+          int t=0;
+          Keyboard.press(KEY_ARROW_UP);
+          delay(50);
+          t+50;
+          case 8:
+          Keyboard.press(KEY_BACKSPACE);
+          delay(50);
+          Keyboard.release(KEY_BACKSPACE);
+          default:
+          break;
+        } //end switch
+        if (i==7){
+          int moving=digitalRead(FWD);
+          if (moving==HIGH){
+            Keyboard.release(FWD);
+          }
         }
-      }
-    }
-  }
-  while (quit_key==HIGH);
-  if ((quit_key==LOW)&&(q_def==HIGH)){
-    delay(30);
-    quit_key=digitalRead(KEY_Q);
-    if quit_key==LOW{ 
-      Keyboard.press(KEY_LEFT_ALT);
-      Keyboard.press(KEY_F4);  //a classic killing technique
-      delay(50);
-      Keyboard.releaseAll();
-      /*Keyboard.println("se ha cerrado el programa");*/
-      Keyboard.end();
-    }
-    else{
-      quit_key==HIGH;
-      q_def=quit_key;
-    }
-  }
+        if (i==3){
+          //Keyboard.println("adiós :(");
+          Keyboard.releaseAll();
+          Keyboard.end();
+        }
+      }   //end if
+    }     //end if
+  }       //end for
+  /*x=analogRead(X);
+  y=analogRead(Y);*/
+  /*float angleRad=0;
+  double tg=y/x*/
 }
