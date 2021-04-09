60716a59282b79d98bea5d3e2c4c6f7dace7b7c4
Amélie <79919225+amelie-nader-prieto@users.noreply.github.com>
2021-04-09 16:29:03 +0200

    Update Teclado_macros.c

--- Teclado_macros/Teclado_macros.c
+++ Teclado_macros/Teclado_macros.c
@@ -3,15 +3,52 @@
 /*#include "Keyboard.h"
 #include "robot.h"
 #include "vector.h"*/
+#include <locale.h>
+
+#define X A0        
+#define Y A1        //joystick pins: analog
+#define PAUSE 2        //(press joystick: pause movement)
+#define QUIT 3     //"quit"
+#define SWITCH 4     //"switch"
+#define START 5  //("start/end" button)
+#define SAVE 6  //("save" button)
+#define FWD 7  //"move" button (which is pressed and held down);
+#define TURN 8  //"return" button
 
 enum perfiles {
 	Perfil1,
 	Perfil2
 };
 
+enum teclas { //digital pins
+	Tecla2 = 2,
+	Tecla3,
+	Tecla4,
+	Tecla5,
+	Tecla6,
+	Tecla7,
+	Tecla8
+};
+
+enum value_keys {
+	HIGH=0,
+	LOW
+};
+
 int main(void) {
-	//configuración (setup);
+	int t;
+	//config (setup);
+	void setup() {
+		;
+	}
 	//bucle principal
+	void loop() {
+		;
+	}
 	//cierre
 	;
+}
+
+void config() {
+	setlocale(LC_ALL,"spanish");
 }
