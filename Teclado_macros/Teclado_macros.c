b03b8c50badbc3ad051e3c37a49274c2cdd756d9
Amélie <79919225+amelie-nader-prieto@users.noreply.github.com>
2021-04-11 09:23:00 +0200

    add typedef and enum variables

--- Teclado_macros/Teclado_macros.c
+++ Teclado_macros/Teclado_macros.c
@@ -1,54 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*#include "Keyboard.h"
-#include "robot.h"
-#include "vector.h"*/
 #include <locale.h>
+#include <time.h>
+#include <signal.h>
 
-#define X A0        
-#define Y A1        //joystick pins: analog
-#define PAUSE 2        //(press joystick: pause movement)
-#define QUIT 3     //"quit"
-#define SWITCH 4     //"switch"
-#define START 5  //("start/end" button)
-#define SAVE 6  //("save" button)
-#define FWD 7  //"move" button (which is pressed and held down);
-#define TURN 8  //"return" button
-
-enum perfiles {
-	Perfil1,
+enum Perfiles {
+	Perfil1=1,
 	Perfil2
 };
 
-enum teclas { //digital pins
-	Tecla2 = 2,
-	Tecla3,
-	Tecla4,
-	Tecla5,
-	Tecla6,
-	Tecla7,
-	Tecla8
+enum Botones { //digital pins
+	pause = 2,
+	quit,
+	profileSwitch,
+	start,
+	save,
+	fwd,
+	turn
 };
 
-enum value_keys {
+typedef enum buttonStates {
 	HIGH=0,
 	LOW
-};
+}state;
+
+void setup() {	//configuracion
+	;
+}
 
 int main(void) {
-	int t;
-	//config (setup);
-	void setup() {
-		;
-	}
+	state button_states[7] = {HIGH,HIGH,HIGH,HIGH,HIGH,HIGH,HIGH};
 	//bucle principal
 	void loop() {
 		;
 	}
 	//cierre
 	;
-}
-
-void config() {
-	setlocale(LC_ALL,"spanish");
 }
