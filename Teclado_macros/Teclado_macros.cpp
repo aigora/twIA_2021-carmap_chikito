f28b7dec446ae8daa158e0a52a3b5a3100bb275c
Amélie <79919225+amelie-nader-prieto@users.noreply.github.com>
2021-04-20 16:12:09 +0200

    Update Teclado_macros.cpp

--- Teclado_macros/Teclado_macros.cpp
+++ Teclado_macros/Teclado_macros.cpp
@@ -4,6 +4,8 @@
 #include <windows.h>
 #include "SerialClass/SerialClass.h"
 
+#define TAM 7
+
 typedef enum buttonPins {
 	pause = 2,quit,profileSwitch,start,save,fwd,turn
 }BUTTONS;
@@ -11,10 +13,17 @@ typedef enum buttonStates {
 	HIGH=0,LOW
 }STATE;
 
+void init(BUTTONS[], STATE[]);
+
 void main() {
-	char buffer;
-	Serial* Arduino;
-	char port[] = "COM3";
+	BUTTONS keypad[TAM]; STATE button_states[TAM];
+	init(keypad,button_states);
+	Serial* Arduino; char port[] = "COM3";
 	Arduino = new Serial((char*)port);
 	while (Arduino->IsConnected) { ; }
+}
+
+void init(BUTTONS[], STATE[]) {
+	BUTTONS keypad[TAM] = {pause,quit,profileSwitch,start,save,fwd,turn};
+	STATE button_states[TAM] = {HIGH,HIGH,HIGH,HIGH,HIGH,HIGH,HIGH};
 }
