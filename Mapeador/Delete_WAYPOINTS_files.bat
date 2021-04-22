53b28754a6ab95e802f274490f49b5bd13d66e13
echedey-luis-alvarez <80125792+echedey-luis-alvarez@users.noreply.github.com>
2021-04-22 23:34:24 +0200

    Create Delete_WAYPOINTS_files.bat
    Deletes temp "waypoints" files generated by the program

---
+++ Mapeador/Delete_WAYPOINTS_files.bat
@@ -0,0 +1,22 @@
+@echo off
+echo This program will DELETE all files with syntax Mapeador_WAYPOINTS_*.txt
+
+echo Would you like to continue [Y/N]?
+
+set /p cont=
+if "%cont%"=="y" goto proceed
+if "%cont%"=="Y" goto proceed
+if "%cont%"=="n" goto ini
+if "%cont%"=="N" goto ini
+goto close
+
+:proceed
+@echo on
+del Mapeador_WAYPOINTS_*.txt
+echo Finished
+pause
+goto close
+
+:close
+@cls&exit
+
