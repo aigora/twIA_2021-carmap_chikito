a0624aa445b2294e45387e776f3efc6eca671860
echedey-luis-alvarez <80125792+echedey-luis-alvarez@users.noreply.github.com>
2021-04-24 22:32:42 +0200

    Añadidos ejemplos relativos a los últimos cambios
    @Fer014 te dejo un ejemplo de escritura y lectura de puntos. Ya tú sabe papi que solo tiene que escribir en el fichero.
    @luc-39 te añadí la función de lo que hablamos; es bastante poco, pero ahí está por si necesitas modificar algo del protocolo. Eso ya lo ves tú cuando te pongas.
    
    Changelog:
    - Añadida función formato del protocolo de tiempo
    - Pequeño cambio de tipo de variable por consistencia
    - Añadidos los ejemplos
    - Eliminada createWaypoints_file() que ahora está en waypoint
    - Pequeños typos

--- Mapeador/Main-mapeador.cpp
+++ Mapeador/Main-mapeador.cpp
@@ -8,10 +8,10 @@
 #include <wchar.h> // Para usar los caracteres amplios (modificación y paso de valores a SerialClass.h)
 #include <iso646.h> // "||" se puede escribir como "or" y "&&" como "and" - legibilidad: https://cplusplus.com/reference/ciso646/
 #include <math.h> // Funciones matemáticas
-#include <time.h> // Funciones para obtener la fecha y hora, y darles formato
 
 #include "vector.h" // Custom library to create and operate vectors easily
 #include "SerialClass/SerialClass.h" // Retrieved from: https://github.com/Gmatarrubia/LibreriasTutoriales (modified local resources)
+#include "waypoints_filehandler.h" // Librería para almacenar la trayectoria del robot en archivos binarios
 
 // Bluetooth definitions
 #define PORT_SZ 15 // Port wide character string size
@@ -19,7 +19,7 @@
 
 // File handling definitions
 #define NAME_SZ 50
-#define WP_PATH "WAYPOINTS" // Unused: in case files are saved into specific folder, use this folder name
+// #define WP_PATH "WAYPOINTS" // Unused: in case files are saved into specific folder, use this folder name
 #define MODE_TIMESTAMP "\0" // How to assign the name to the waypoint files
 
 // Math definitions
@@ -28,18 +28,16 @@
 
 //// Program functions ////
 
-// File & folder oriented
-errno_t createWaypoints_file(FILE*, const size_t, const char[], char*); /* Devuelve cero si consigue hacer el archivo [devuelve lo mismo que fopen_s()]; args.:
-    puntero a archivo (se modifica),
-    tamaño max del nombre archivo, 
-    nombre archivo o protocolo a utilizar (MODE_TIMESTAMP ó "\0" para usar la estampa de tiempo), 
-    puntero a nombre asignado al archivo finalmente, tamaño mayor o igual al especificado en arg. nº 2
-    */
-
 // Bluetooth oriented
-void getCOM_port_s(wchar_t*, size_t); /* Obtiene puerto COM desde stdin; args.:
+void getCOM_port_s(wchar_t*, size_t);
+    /* Obtiene puerto COM desde stdin; args.:
     puntero a cadena de caracteres anchos donde se guarda el puerto a utilizar
     tamaño max de la cadena
+    */
+
+errno_t command_Arduino_time_s(char* buffer, size_t sz, int left_wheel, int right_wheel);
+    /* Genera comando con formato "t:+-[tiempo motor izq],+-[tiempo motor der];"
+    el tamaño mínimo de buffer (sz) es 20, donde los tiempos están en milisegundos y no exceden 9999
     */
 
 int main() // Main function
@@ -54,7 +52,7 @@ int main() // Main function
 
     // Initialize "waypoints file" variables
     FILE* fp_puntos = NULL;
-    char* nombreArchivoPuntos = (char*)calloc(NAME_SZ, sizeof(byte));
+    char* nombreArchivoPuntos = (char*)calloc(NAME_SZ, sizeof(char));
 
     getCOM_port_s(puerto, PORT_SZ); // Menú que obtiene el nombre del puerto a usar
     printf("\nLa comunicacion se realizara a traves del puerto %ws\n", puerto); // Imprime en pantalla el puerto que se está usando
@@ -63,7 +61,7 @@ int main() // Main function
 
     if (Arduino->IsConnected()) { // Solo si el Arduino se puede conectar, se crea un archivo para guardar el progreso
         // Initializing file to be written
-        errno_t error = createWaypoints_file(fp_puntos, NAME_SZ, MODE_TIMESTAMP, nombreArchivoPuntos);
+        errno_t error = waypts_create_file(&fp_puntos, NAME_SZ, MODE_TIMESTAMP, nombreArchivoPuntos);
 
         if (error == 100) { // Fallo asignando memoria dinámica
             printf("Error in memory assignment. Big oof for the programmer trying to figure this out.");
@@ -82,7 +80,7 @@ int main() // Main function
             printf("Error creating/opening file. File might already exist. Error number: %d", error);
             return 35;
         }
-        else { // Arduino esstá conectado y el fichero fue creado sin errores
+        else { // Arduino está conectado y el fichero fue creado sin errores
             printf("El progreso del robot sera guardado en el fichero: %s", nombreArchivoPuntos);
         }
     }
@@ -91,6 +89,34 @@ int main() // Main function
         return 40;
     }
 
+    // Ejemplo para esciribir un vector o punto cualquiera en el archivo binario //
+    printf("\nEjemplo escritura en fichero: escribiendo puntos (0,0) y (1,1)");
+    vector2D vector = { 0 , 0 }; // Iniciamos vector
+    waypts_bappend_vect(fp_puntos, &vector, NULL); // Escribimos sin retornar el número de elementos escritos
+
+    size_t num_de_puntos_en_fichero;
+    vector = { 1, 1 };
+    waypts_bappend_vect(fp_puntos, &vector, &num_de_puntos_en_fichero); // Ahora sí retornamos nº de puntos escritos a num_de_puntos_en_fichero
+    // Ahora num_de_puntos_en_fichero debe ser 2;
+    printf("\nEl numero de puntos en el fichero es: %u\nLeyendo datos escritos...\nPunto 1: ", num_de_puntos_en_fichero);
+
+    fseek(fp_puntos, sizeof(size_t), SEEK_SET); // Ponemos el cursor al inicio de los puntos, no del fichero
+    waypts_bread_vect(fp_puntos, &vector); // Hacemos la lectura de un elemento
+    vector2D_print(vector, DEF_PRECISION); // Imprimimos elemento
+
+    printf("\nPunto 2: "); // Idem anterior, solo que el cursor del búfer ya está colocado para leer el siguiente punto
+    waypts_bread_vect(fp_puntos, &vector);
+    vector2D_print(vector, DEF_PRECISION);
+    // Fin del ejemplo //
+
+    // Ejemplo que envía comando de movimiento por tiempo al Arduino //
+    char mov_ruedas[50]; // Creamos una string de al menos 20 chars. Ver descripción en la declaración
+    command_Arduino_time_s(mov_ruedas, sizeof(mov_ruedas), 750, -100); // Tiempos en milisegundos, el sentido depende del signo (positivo = hacia delante)
+    Arduino->WriteData(mov_ruedas, strlen(mov_ruedas));
+    printf("\nHemos enviado al Arduino el comando:\n%s", mov_ruedas);
+    // Fin del ejemplo //
+
+    // Ejemplo de Moodle para la conexión Bluetooth
     while (Arduino->IsConnected()) // Mientras el Arduino esté conectado se puede proceder
     {
         if (contador == 0)
@@ -124,32 +150,6 @@ int main() // Main function
     return 0; // Exit without errors
 }
 
-errno_t createWaypoints_file(FILE* fp, size_t tam_nombre, const char proto_or_name[], char* filename)
-{
-    char* nombreArchivo = (char*)calloc(tam_nombre, sizeof(byte));
-    if (nombreArchivo == NULL) {
-        return 100; // Error cuando no se puede asignar memoria
-    }
-
-    // Se genera o copia el nombre para el archivo
-    if (proto_or_name == "\0") { // Modo en el que se asigna la estampa del tiempo al archivo para hacerlo unico
-        time_t localTime = time(NULL); // Toma el tiempo actual del PC
-        struct tm localTimeTM;
-        localtime_s(&localTimeTM, &localTime); // Se pasa a estructura con datos separados en año, mes, día, hora, minuto y segundo
-        strftime(nombreArchivo, tam_nombre, "Mapeador_WAYPOINTS_%F_%H-%M-%S.txt", &localTimeTM); // Crea nombre del archivo, e.g., "Mapeador_WAYPOINTS_2021-04-17_20-32-17.txt"
-    }
-    else if (strlen(proto_or_name) > 1 ) { // Si el nombre ya es especificado en el argumento
-        strcpy_s(nombreArchivo, tam_nombre, proto_or_name);
-    }
-    else {
-        fprintf(stderr, "Cannot assign filename to Waypoints file.");
-        return 150; // Error cuando no se puede asiganr un nombre decente
-    }
-
-    strcpy_s(filename, tam_nombre, nombreArchivo); // Se copia el nombre de archivo asignado a la cadena que contiene el nombre
-    return fopen_s(&fp, nombreArchivo, "w"); // Se crea el archivo en modo escritura, se devuelve error de esta función
-}
-
 void getCOM_port_s(wchar_t* dest, size_t max) {
     unsigned char status = 0, portNumber = 0;
     char tempPort[PORT_SZ]; // Trabajamos en cadena de caracteres simples y luego pasamos a *wchar_t
@@ -170,4 +170,10 @@ void getCOM_port_s(wchar_t* dest, size_t max) {
     swprintf_s(dest, PORT_SZ, L"%hs", tempPort); // Cambia el formato de *char a *wchar_t, escribe en cadena destino
     
     return; // Fin de obtener puerto.
+}
+
+errno_t command_Arduino_time_s(char* buffer, size_t sz, int left_wheel, int right_wheel) {
+    if (sz < 20) return STRUNCATE;
+    sprintf_s(buffer, sz, "t:%+0.4d,%+0.4d;", left_wheel, right_wheel);
+    return 0;
 }
