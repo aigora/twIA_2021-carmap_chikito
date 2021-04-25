1dfcef13088e35d86062abb23ef9177cb3c17135
Amélie <79919225+amelie-nader-prieto@users.noreply.github.com>
2021-04-25 16:54:01 +0200

    add functions to main

--- Teclado_macros/Source/Teclado_macros.cpp
+++ Teclado_macros/Source/Teclado_macros.cpp
@@ -4,7 +4,7 @@
 #include <windows.h>
 #include "SerialClass/SerialClass.h"
 
-#define TAM 7
+#define TAM 6
 
 typedef enum buttonPins {
 	pause = 2,quit,profileSwitch,start,save,fwd,turn
@@ -14,16 +14,30 @@ typedef enum buttonStates {
 }STATE;
 
 void init(BUTTONS[], STATE[]);
+void connect(Serial* Arduino);
+void read(char buffer[],int nBytes);
 
 void main() {
+	Serial* Arduino; Keyboard_* Keyboard;
 	BUTTONS keypad[TAM]; STATE button_states[TAM];
-	init(keypad,button_states);
-	Serial* Arduino; char port[] = "COM3";
-	Arduino = new Serial((char*)port);
-	while (Arduino->IsConnected) { ; }
+	char port[] = "COM3"; char buffer[31];
+	int nBytes;
+	init(keypad, button_states); connect(Arduino);
+	while (Arduino->IsConnected) { 
+		nBytes=Arduino->ReadData(buffer,sizeof(buffer));
+		if (nBytes > sizeof(buffer)) nBytes = sizeof(buffer) - 1;
+		nBytes != -1 ? read(buffer, nBytes):printf("nothing happened?\n");
+		Sleep(500);
+	}
 }
 
-void init(BUTTONS[], STATE[]) {
-	BUTTONS keypad[TAM] = {pause,quit,profileSwitch,start,save,fwd,turn};
-	STATE button_states[TAM] = {HIGH,HIGH,HIGH,HIGH,HIGH,HIGH,HIGH};
+void init(BUTTONS[], STATE[]) { //these are the pins in the arduino which are used
+	BUTTONS keypad[TAM] = {pause,quit,start,save,fwd,turn};
+	STATE button_states[TAM] = {HIGH,HIGH,HIGH,HIGH,HIGH,HIGH};
+}
+void connect(Serial* Arduino) { //connecting arduino to COM3 port
+	Arduino = new Serial((char*)port);
+}
+void read(char buffer[], int nBytes) {
+	buffer[nBytes-1]='\0';
 }
