992bebbddedc61bc3e02510f8362168ce42965f7
celia-t22 <celia.torrecillas@alumnos.upm.es>
2021-04-26 23:52:37 +0200

    Update mototres
    He corregido los errores del anterior programa, no esta acabado y queda por hacer.

--- Robot_CMC/Robot_CMC.ino
+++ Robot_CMC/Robot_CMC.ino
@@ -1,11 +1,14 @@
+/*  Project: CarMap - Chikito
+ *  This file is the main program for the robot Chikito.
+ */
+#define motorIZQ1 6
+#define motorDER1 7
 
-#define motorIZQ1=6;
-#define motorDER1=7;
-
-#define motorIZQ2=8;
-#define motorDER2=9;
+#define motorIZQ2 8
+#define motorDER2 9
 
 void setup() {
+  // put your setup code here, to run once:
   pinMode(motorIZQ1,OUTPUT);
   pinMode(motorDER1,OUTPUT);
   pinMode(motorIZQ2,OUTPUT);
@@ -13,6 +16,33 @@ void setup() {
 
 }
 
+//PROTOTIPOS DE FUNCIÓN
+void parar();
+void delante();
+void atras();
+void derecha();
+void izquierda();
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  parar();
+  delay(3000); //milisegundos
+
+  delante();
+  delay(3000); //milisegundos
+  
+  atras();
+  delay(3000); //milisegundos
+
+  derecha();
+  delay(3000); //milisegundos
+
+  izquierda();
+  delay(3000); //milisegundos
+
+}
+
+//CUERPOS DE FUNCIONES
 void parar()
 {
   digitalWrite(motorIZQ1,LOW);
@@ -52,22 +82,3 @@ void izquierda()
   digitalWrite(motorIZQ2,LOW);
   digitalWrite(motorDER2,HIGH);
 }
-
-void loop() {
-  parar();
-  delay(); //milisegundos
-
-  delante();
-  delay(); //milisegundos
-  
-  atras();
-  delay(); //milisegundos
-
-  derecha();
-  delay(); //milisegundos
-
-  izquierda();
-  delay(); //milisegundos
-
-}
-
