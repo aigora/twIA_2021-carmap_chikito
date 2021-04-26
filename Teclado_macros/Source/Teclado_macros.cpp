ee6c63fc4b52aab6c77098e0c2b3b13eb1c8f424
Amélie <79919225+amelie-nader-prieto@users.noreply.github.com>
2021-04-26 06:51:49 +0200

    Update Teclado_macros.cpp

--- Teclado_macros/Source/Teclado_macros.cpp
+++ Teclado_macros/Source/Teclado_macros.cpp
@@ -8,36 +8,59 @@
 
 typedef enum buttonPins {
 	pause = 2,quit,profileSwitch,start,save,fwd,turn
-}BUTTONS;
-typedef enum buttonStates {
-	HIGH=0,LOW
-}STATE;
+}BUTTONS;  //buttons on the board
 
-void init(BUTTONS[], STATE[]);
+/*functions' prototypes*/
+void init(BUTTONS[],*Keyboard_);
 void connect(Serial* Arduino);
+int getSignal(Serial* Arduino, char buffer[]);
 void read(char buffer[],int nBytes);
+void press(int key);
+void leave(*Serial,*Keybaord_);
 
 void main() {
 	Serial* Arduino; Keyboard_* Keyboard;
-	BUTTONS keypad[TAM]; STATE button_states[TAM];
+	BUTTONS keypad[TAM];
 	char port[] = "COM3"; char buffer[31];
 	int nBytes;
-	init(keypad, button_states); connect(Arduino);
+	init(keypad, button_states,Keyboard); connect(Arduino);
 	while (Arduino->IsConnected) { 
-		nBytes=Arduino->ReadData(buffer,sizeof(buffer));
-		if (nBytes > sizeof(buffer)) nBytes = sizeof(buffer) - 1;
-		nBytes != -1 ? read(buffer, nBytes):printf("nothing happened?\n");
+		nBytes = getSignal(Arduino, buffer);
+		read(buffer, nBytes);
 		Sleep(500);
 	}
 }
 
-void init(BUTTONS[], STATE[]) { //these are the pins in the arduino which are used
+/*initializes enum vectors (buttons on keypad ) and keyboard*/
+void init(BUTTONS[], STATE[],Keyboard) { 
 	BUTTONS keypad[TAM] = {pause,quit,start,save,fwd,turn};
-	STATE button_states[TAM] = {HIGH,HIGH,HIGH,HIGH,HIGH,HIGH};
+	Keybpard->begin();
 }
-void connect(Serial* Arduino) { //connecting arduino to COM3 port
+
+/*set up connection with Arduino in COM3 port*/
+void connect(Serial* Arduino) {
 	Arduino = new Serial((char*)port);
 }
+
+/*looks in buffer memory and returns the number of bytes received*/
+int getSignal(Serial* Arduino, char buffer[]) {
+	int nBytes;
+	nBytes=Arduino->ReadData(buffer, sizeof(buffer));
+	if (nBytes > sizeof(buffer)) nBytes = sizeof(buffer) - 1;
+	nBytes != -1 ? read(buffer, nBytes) : printf("nothing happened?\n");
+	return nBytes;
+}
+
+/*the first (int nBytes) elements of string are read*/
 void read(char buffer[], int nBytes) {
-	buffer[nBytes-1]='\0';
+	buffer[nBytes-1]='\0';  //char string ends here
+}
+
+/*a key in computer's keyboard is pressed when button in arduino board is pressed*/
+void press(int key) {
+	;
+}
+
+void leave(*Serial, *Keyboard) {
+	;
 }
