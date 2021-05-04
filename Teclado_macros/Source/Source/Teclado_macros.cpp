bf9690cae031cf66e01da2db8874abc5c0db363c
Amélie <79919225+amelie-nader-prieto@users.noreply.github.com>
2021-05-04 22:02:52 +0200

    Update Teclado_macros.cpp

--- Teclado_macros/Source/Source/Teclado_macros.cpp
+++ Teclado_macros/Source/Source/Teclado_macros.cpp
@@ -4,23 +4,27 @@
 #include "SerialClass/SerialClass.h"
 #include "Keyboard/Keyboard.h"
 
-#define TAM 6
 #define BUFF 200
 
 /*functions' prototypes*/
 void connect(Serial*,char []);
-void pressKey(int);
 void endConnection(Serial*);
+void press(int);
 
 void main() {
 	Serial* Arduino; Keyboard_* keyboard;
 	char port[] = "COM3", buffer[BUFF];
-	int nBytes,key;
+	const char key_N[]="N",key_S[]="S",key_E[]="E",key_W[]="W";
+	int nBytes=0,key=0;
 	connect(Arduino, port);
 	while (Arduino->IsConnected()) { 
-		nBytes=Arduino->ReadData((char*)buffer,nBytes);
+		nBytes=Arduino->ReadData(buffer,sizeof(char)*nBytes);
 		Sleep(500);
+		if(nBytes!=-1){ 
+			buffer[nBytes]='\0';
+		}
 	}
+	endConnection(Arduino);
 }
 
 /*set up connection with Arduino in COM3 port*/
@@ -28,20 +32,20 @@ void connect(Serial* Arduino,char port[]) {
 	Arduino = new Serial((char*)port);
 }
 
-/*a key in computer's keyboard is pressed when button in arduino board is pressed*/
-void pressKey(int) {
-	int key; switch (key) {
-	case 2: Keyboard->press(KEY_ESC); break;
-	case 3: leave(); break;  //pressing this button executes the leave function and closes the program;;
-	case 4: /*KEY ENTER*/ break,
-	case 5: Keyboard->press(KEY_RIGHT_CTRL); Keyboard->write('s'); break;
-	case 6: /*movement's function*/ break;
-	case 7: Keyboard->(KEY_BACKSPACE); break;
-	default: break;
-	}
-}
-
 /*ends connections and closes the program*/
 void endConnection(Serial* Arduino) { 
 	Arduino->~Serial();
+}
+
+void press(int){
+	case 1:break;
+	case 2:break;
+	case 3:break;
+	case 4:break;
+	case 5:break;
+	case 6:break;
+	case 7:break;
+	case 8:break;
+	case 9:break;
+	default:break;
 }
