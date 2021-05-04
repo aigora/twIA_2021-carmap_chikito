0b97c4c0fdcc5132e5f5cd997ea183edb44c86e1
celia-t22 <celia.torrecillas@alumnos.upm.es>
2021-05-04 18:12:50 +0200

    actualización código motores
    Movimiento motores, nuevas funciones

--- Robot_CMC/Robot_CMC.ino
+++ Robot_CMC/Robot_CMC.ino
@@ -1,9 +1,11 @@
 /*  Project: CarMap - Chikito
  *  This file is the main program for the robot Chikito.
  */
+ //MOTOR 1 ES EL MOTOR DE LA DERECHA
 #define motorIZQ1 6
 #define motorDER1 7
 
+//MOTOR 2 ES EK MOTOR DE LA IZQUIERDA
 #define motorIZQ2 8
 #define motorDER2 9
 
@@ -11,17 +13,28 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(motorIZQ1,OUTPUT);
   pinMode(motorDER1,OUTPUT);
+  
   pinMode(motorIZQ2,OUTPUT);
   pinMode(motorDER2,OUTPUT);
 
 }
 
-//PROTOTIPOS DE FUNCIÓN
+//PROTOTIPOS DE FUNCIÓN DE MOVIMIENTO
 void parar();
 void delante();
 void atras();
 void derecha();
 void izquierda();
+
+//PROTOTIPOS DE FUNCIÓN DE MOTORES
+void motor1DER();
+void motor1IZQ();
+
+void motor2DER();
+void motor2IZQ();
+
+void motor1PARA();
+void motor2PARA();
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -42,43 +55,71 @@ void loop() {
 
 }
 
-//CUERPOS DE FUNCIONES
+//CUERPOS DE FUNCIONES DE MOVIMIENTO
 void parar()
 {
-  digitalWrite(motorIZQ1,LOW);
-  digitalWrite(motorDER1,LOW);
-  digitalWrite(motorIZQ2,LOW);
-  digitalWrite(motorDER2,LOW);
+  motor1PARA();  
+  motor2PARA();
 }
 
 void delante()
 {
-  digitalWrite(motorIZQ1,LOW);
-  digitalWrite(motorDER1,HIGH);
-  digitalWrite(motorIZQ2,HIGH);
-  digitalWrite(motorDER2,LOW);
+  motor1DER();
+  motor2IZQ();
 }
 
 void atras()
 {
-  digitalWrite(motorIZQ1,HIGH);
-  digitalWrite(motorDER1,LOW);
-  digitalWrite(motorIZQ2,LOW);
-  digitalWrite(motorDER2,HIGH);
+  motor1IZQ();
+  motor2DER();
 }
 
 void derecha()
 {
-  digitalWrite(motorIZQ1,LOW);
-  digitalWrite(motorDER1,HIGH);
-  digitalWrite(motorIZQ2,HIGH);
-  digitalWrite(motorDER2,LOW);
+  motor1DER();  
+  motor2DER();
 }
 
 void izquierda()
 {
+  motor1IZQ();
+  motor2IZQ();
+}
+
+//CUERPOS DE FUNCIONES DE MOTORES
+void motor1DER()
+{
+  digitalWrite(motorIZQ1,LOW);
+  digitalWrite(motorDER1,HIGH);
+}
+
+void motor1IZQ()
+{
   digitalWrite(motorIZQ1,HIGH);
   digitalWrite(motorDER1,LOW);
+}
+
+void motor2DER()
+{
   digitalWrite(motorIZQ2,LOW);
   digitalWrite(motorDER2,HIGH);
+}
+
+void motor2IZQ()
+{
+  digitalWrite(motorIZQ2,HIGH);
+  digitalWrite(motorDER2,LOW);
+ 
+}
+
+void motor1PARA()
+{
+  digitalWrite(motorIZQ1,LOW);
+  digitalWrite(motorDER1,LOW);
+}
+
+void motor2PARA()
+{
+  digitalWrite(motorIZQ2,LOW);
+  digitalWrite(motorDER2,LOW); 
 }
