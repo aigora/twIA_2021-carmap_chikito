b17fabc26119691a9b115e060adb90355693eaef
celia-t22 <celia.torrecillas@alumnos.upm.es>
2021-05-04 19:00:01 +0200

    Revert "Merge branch 'master' of https://github.com/aigora/twIA_2021-carmap_chikito"
    This reverts commit 7f08e69d75f11177c7c935a2e1b4be8d9dfd47e6, reversing
    changes made to 719d847e9fd0666a87860a5b3fe81cef5ae154fd.

--- Teclado_macros/Teclado_macros/Teclado_macros.ino
+++ Teclado_macros/Teclado_macros/Teclado_macros.ino
@@ -3,47 +3,40 @@
  *  This file is the main program for the macro keyboard.
  */
 
-/*extra buttons;*/
-/*#define PAUSE 2        
-#define QUIT 3 
-#define START 4  
-#define SAVE 5  
-#define TURN 6*/
-/*"north/south/east/west" buttons: press to move robot*/
-#define N 7
-#define S 8
-#define E 9
-#define W 10
+/*pins of controller used by pushbuttons*/
+#define PAUSE 2        //press to stop robot's movement; then press to move again.
+#define QUIT 3     //stops and closes Main program
+#define START 4  //starts the engines and begins recording of robot's path; press again to end
+#define SAVE 5  //sends data of robot's position to Main program (which draws the path)
+#define N 6  //press or hold down to keep robot moving forward
+#define TURN 7  //ends recording of the path; robot turns around and returns that same path
+/*#define S 8
+#define W 9
+#define E 10*/
 
-/*initializes Serial and sets button pins as input*/
 void setup() {
   Serial.begin(9600); while(!Serial){;}
-  const int button_pins[]={PAUSE,QUIT,START,SAVE,TURN,N,S,E,W};
+  const int button_pins[]={PAUSE,QUIT,START,SAVE,N,TURN};
   int size=(sizeof(button_pins)/sizeof(int)),k=0;
-  for (k=0;k<size;k++){
-    pinMode(button_pins[k],INPUT);
-    }
+  for (k=0;k<size;k++){pinMode(button_pins[k],INPUT);}
 }
 
-/*push buttons trigger the corresponding Macros when pressed by sending a char or string through Serial*/
+/*main program which runs while arduino is connected*/
 void loop(){
-  const int buttons[]={PAUSE,QUIT,START,SAVE,TURN,N,S,E,W};
+  const int buttons[]={PAUSE,QUIT,START,SAVE,N,TURN};
   int size=(sizeof(buttons)/sizeof(int)),i=0;
   int buttons_now[]={HIGH,HIGH,HIGH,HIGH,HIGH,HIGH};
   for (i=0;i<size;i++){
     buttons_now[i]=digitalRead(buttons[i]);
-    if (buttons_now[i]==LOW){
+    if (buttons_now[i]==LOW){ //prints message through serial port
       switch(i){
-        case 2:break;
-        case 3:break;
-        case 4:break;
-        case 5:break;
-        case 6:break;
-        case 7:Serial.println(N); break;
-        case 8:Serial.println(S); break;
-        case 9:Serial.println(E); break;
-        case 10:Serial.println(W); break;
-        default:break;
+        case 2: Serial.println("pause"); break;
+        case 3: Serial.println("leave"); break;
+        case 4: Serial.println("start"); break;
+        case 5: Serial.println("send_point: "); /*also prints/sends coordinates of robot*/ break;
+        case 6: Serial.println("go_north"); break;
+        case 7: Serial.println("go_back"); break;
+        default: Serial.println(); break;
         }
       }
     } delay(500);
