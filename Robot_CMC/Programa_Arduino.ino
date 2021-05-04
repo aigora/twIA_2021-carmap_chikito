7275ade16275d07ee606d49450a0ec4bbbcda1c4
luc-39 <lucas.sanchez.solera@alumnos.upm.es>
2021-05-04 18:05:21 +0200

    Update Programa_Arduino.ino

--- Robot_CMC/Programa_Arduino.ino
+++ Robot_CMC/Programa_Arduino.ino
@@ -6,35 +6,59 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
- int tiempo[2]={0000,0000}; //Declaración de la variable tiempo en ms
- 
  if(Serial.available()>0) //Si hay datos disponibles del PC
- {
-  String cadena=Serial.readStringUntil('\n'); //Lectura de la cadena
+  procesar_mensaje();
+}
 
-  cadena.toUpperCase(); //Convertir a mayúsculas
-  
-  for (int i=0;i<cadena.length();i++) //Recorrido de la cadena
-  {
-   Serial.print(cadena.charAt(i)); //Enviar caracter a caracter
-   delay(50);
-  }
-  Serial.println();
- }
- delay(50);
+void procesar_mensaje(void)
+{
+ int tMotorIZQ1=0000, tMotorIZQ2=0000, tMotorDER1=0000, tMotorDER2=0000;
  
- while(Serial.available()>0) //Si hay datos disponibles del PC
- {
-  int inInt=Serial.read();
-   String cadena=Serial.readStringUntil('\n'); //Lectura de la cadena
-   if(isDigit(inInt))
-   {
-    cadena+=(int)inInt;
-   }
- }
+ String cadena=Serial.readStringUntil('\n'); //Lee el mensaje
+ String valor1=Serial.readStringUntil('\n'); //Lee el valor del motor izq 1
+ String valor2=Serial.readStringUntil('\n'); //Lee el valor del motor izq 2
+ String valor3=Serial.readStringUntil('\n'); //Lee el valor del motor dcha 1
+ String valor4=Serial.readStringUntil('\n'); //Lee el valor del motor dcha 2
  
- if(tiempo>-9999 && tiempo<9999)
+ tMotorIZQ1=valor1.toInt(); //Transforma a valor entero valor1
+ tMotorIZQ2=valor2.toInt(); //Transforma a valor entero valor2
+ tMotorDER1=valor3.toInt(); //Transforma a valor entero valor3
+ tMotorDER2=valor4.toInt(); //Transforma a valor entero valor4
+ 
+ if(-9999>=tMotorIZQ1<=9999) 
  {
   
  }
+ else
+ {
+  Serial.print("Error");
+ }
+  
+ if(-9999>=tMotorIZQ2<=9999)
+ {
+  
+ }
+ else
+ {
+  Serial.print("Error");
+ }
+
+ if(-9999>=tMotorDER1<=9999)
+ {
+  
+ }
+ else
+ {
+  Serial.print("Error");
+ }
+
+ if(-9999>=tMotorDER2<=9999)
+ {
+  
+ }
+ else
+ {
+  Serial.print("Error");
+ }
+ 
 }
