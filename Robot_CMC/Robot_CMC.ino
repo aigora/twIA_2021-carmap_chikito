543e6fe59ef9611b5251ed038d1e67da5556ccd5
celia-t22 <celia.torrecillas@alumnos.upm.es>
2021-05-06 23:17:35 +0200

    Arduino completo
    Programa arduino operativo

--- Robot_CMC/Robot_CMC.ino
+++ Robot_CMC/Robot_CMC.ino
@@ -1,32 +1,41 @@
 /*  Project: CarMap - Chikito
  *  This file is the main program for the robot Chikito.
  */
- //MOTOR 1 ES EL MOTOR DE LA DERECHA
+// Referencas a Bibliotecas
+
+// Enumeraciones que facilitan la lectura del código
+
+// Variables globales
+
+//Motor 1 es el motor de la derecha
 #define motor1_IZQ 6
 #define motor1_DER 7
 
-//MOTOR 2 ES EL MOTOR DE LA IZQUIERDA
+//Motor 2 es el motor de la izquierda
 #define motor2_IZQ 8
 #define motor2_DER 9
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
+  // Tareas de configuración 
+  Serial.begin(9600);
+
   pinMode(motor1_IZQ,OUTPUT);
   pinMode(motor1_DER,OUTPUT);
   
   pinMode(motor2_IZQ,OUTPUT);
   pinMode(motor2_DER,OUTPUT);
-
 }
 
-//PROTOTIPOS DE FUNCIÓN DE MOVIMIENTO
+//Prototipos de función de movimiento
 void parar();
 void delante();
 void atras();
 void derecha();
 void izquierda();
 
-//PROTOTIPOS DE FUNCIÓN DE MOTORES
+//Prototipos de función de motores
 void motor1DER();
 void motor1IZQ();
 
@@ -36,26 +45,81 @@ void motor2IZQ();
 void motor1PARA();
 void motor2PARA();
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-  parar();
-  delay(3000); //milisegundos
+  if (Serial.available() > 0)  // Si hay mensajes procedentes del PC  
+    procesar_accion();
+    
+  delay(50);
+} 
 
-  delante();
-  delay(3000); //milisegundos
-  
-  atras();
-  delay(3000); //milisegundos
+// Resto de acciones 
 
-  derecha();
-  delay(3000); //milisegundos
-
-  izquierda();
-  delay(3000); //milisegundos
-
+int procesar_accion(void)
+{
+  unsigned int tiempoMilis=0;
+  char movimiento[4],sentido[4],tiempo[6];
+  String cadena = Serial.readStringUntil('\n'); // Lee mensaje
+  if(cadena[0]=='t' && cadena[1]==':') 
+  {
+    for(int i=0;i<3;i++)
+    {
+      movimiento[i]=cadena[i+2];
+    }
+    movimiento[3]=0;
+    for(int j=0;j<3;j++)
+    {
+      sentido[j]=cadena[j+6];
+    }
+    sentido[3]=0;
+    for(int k=0;k<5;k++)
+    {
+      tiempo[k]=cadena[k+11];
+    }
+    tiempo[5]=0;
+    tiempoMilis= atoi(tiempo);
+    if(movimiento=="lin")
+    {
+      if(sentido=="del"){
+        delante(); //Movimiento lineal hacia delante
+      }
+      else if(sentido=="atr"){
+        atras(); //Movimiento lineal hacia atrás
+        
+      }
+      else{
+      //Error
+      return 20;  
+      }
+    }
+    else if(movimiento=="rot"){
+      if(sentido=="der"){
+        derecha(); //Movimiento rotacional hacia la derecha
+      }
+      else if(sentido=="izq"){
+        izquierda(); //Movimiento rotacional hacia la izquierda
+      }
+      else{
+        //Error
+        return 15;
+      }
+    }
+    else{
+      //Error
+      return 10;
+    }
+  }
+  else{
+    //Error
+    return 1;
+  }
+  delay(tiempoMilis);
+  parar(); //Parar
+  return 0;
 }
 
-//CUERPOS DE FUNCIONES DE MOVIMIENTO
+//Cuerpos de funciones de movimiento
 void parar()
 {
   motor1PARA();  
@@ -86,7 +150,7 @@ void izquierda()
   motor2IZQ();
 }
 
-//CUERPOS DE FUNCIONES DE MOTORES
+//Cuerpos de funciones de motores
 void motor1DER()
 {
   digitalWrite(motor1_IZQ,LOW);
