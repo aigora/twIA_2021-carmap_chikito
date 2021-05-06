223bea974aaa97d206f2f8ad6c0049cd017103f3
Amélie <79919225+amelie-nader-prieto@users.noreply.github.com>
2021-05-07 00:18:04 +0200

    update Arduino code with Keyboard functions

--- Teclado_macros/Teclado_macros/Teclado_macros.ino
+++ Teclado_macros/Teclado_macros/Teclado_macros.ino
@@ -3,48 +3,61 @@
  *  This file is the main program for the macro keyboard.
  */
 
-/*extra buttons;*/
-/*#define PAUSE 2        
-#define QUIT 3 
-#define START 4  
-#define SAVE 5  
-#define TURN 6*/
+#include <Keyboard.h>
+
 /*"north/south/east/west" buttons: press to move robot*/
 #define N 7
 #define S 8
 #define E 9
-#define W 10
+#define O 10
 
 /*initializes Serial and sets button pins as input*/
 void setup() {
   Serial.begin(9600); while(!Serial){;}
-  const int button_pins[]={PAUSE,QUIT,START,SAVE,TURN,N,S,E,W};
-  int size=(sizeof(button_pins)/sizeof(int)),k=0;
-  for (k=0;k<size;k++){
-    pinMode(button_pins[k],INPUT);
-    }
-}
-
-/*push buttons trigger the corresponding Macros when pressed by sending a char or string through Serial*/
-void loop(){
-  const int buttons[]={PAUSE,QUIT,START,SAVE,TURN,N,S,E,W};
-  int size=(sizeof(buttons)/sizeof(int)),i=0;
-  int buttons_now[]={HIGH,HIGH,HIGH,HIGH,HIGH,HIGH};
-  for (i=0;i<size;i++){
-    buttons_now[i]=digitalRead(buttons[i]);
-    if (buttons_now[i]==LOW){
-      switch(i){
-        case 2:break;
-        case 3:break;
-        case 4:break;
-        case 5:break;
-        case 6:break;
-        case 7:Serial.println(N); break;
-        case 8:Serial.println(S); break;
-        case 9:Serial.println(E); break;
-        case 10:Serial.println(W); break;
-        default:break;
-        }
-      }
-    } delay(500);
+  const int arrowpad[]={N,S,E,O}; int i;
+  for(i=0;i<(sizeof(arrowpad)/sizeof(int));i++){ pinMode(arrowpad[i],INPUT_PULLUP); }
+  Keyboard.begin();
   }
+
+/*push buttons trigger the corresponding Macros when pressed*/
+void loop(){
+  const int buttons[]={N,S,E,O};
+  int buttons_states[]={HIGH,HIGH,HIGH,HIGH},buttons_now=HIGH,i;
+  for(i=0;i<(sizeof(buttons_states)/sizeof(int));i++){
+    buttons_now=digitalRead(buttons[i]);
+    if(buttons_now==LOW&&buttons_states[i]==HIGH) {
+      delay(50); buttons_now=digitalRead(i);
+      if(buttons_now==LOW) key(i+1);
+      }
+    }
+  }
+
+void key(int i){
+  switch(i){
+    case 1:north();break;
+    case 2:south();break;
+    case 3:east();break;
+    case 4:west();break;
+    default:break;
+    }
+  }
+void north(){
+  Keyboard.press('N');
+  delay(500);
+  Keyboard.release('N');
+  }
+void south(){
+  Keyboard.press('S');
+  delay(500);
+  Keyboard.release('S');
+  }
+void east(){
+  Keyboard.press('E');
+  delay(500);
+  Keyboard.release('E');
+  }
+void west(){
+  Keyboard.press('O');
+  delay(500);
+  Keyboard.release('O');
+}
